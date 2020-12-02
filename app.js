227a0f70071606982ee41476826e4c1c27968953
DaphneFlapper <daphne.flapper.ap@gmail.com>
2020-12-02 08:31:14 +0100

    1.2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var vm = new Vue({
 	el: '#app',
 
+
 	data: function () {
 		return {
 			search: null,
@@ -8,10 +9,12 @@ var vm = new Vue({
 			playerVars: {
 				autoplay: 1
 			},
-			counter: 0,
-
+			queue: [],
+			object: [],
 		};
 	},
+
+
 
 	computed: {
 		player() {
@@ -23,13 +26,22 @@ var vm = new Vue({
 		searchVideos: function() {
 			var self = this;
 			var search = encodeURI(this.search);
+			var results;
 
 			axios.get('https://vuetv.acmoore.co.uk/search/'+search).then(function (response) {
-				var first_result = response.data[0];
-				self.loadVideo(first_result.video_id);
-
+				self.loadVideo(response.data[0].video_id);
+				//self.cueVideo(second_result.video_id);
+				var i = 0;
+				while (i < 10) {
+					self.object.push(response.data[i]);
+					i++
+				}
+				var t = 0;
+				while (t < 10) {
+					self.cueVideo(response.data[t].video_id);
+					t++
+				}
 			});
-
 		},
 
 		loadVideo: function (video_id) {
@@ -45,12 +57,28 @@ var vm = new Vue({
 		},
 
 		nextVideo: function() {
-			this.player.nextVideo();
+			var next_video = this.queue.shift();
+
+			this.loadVideo(next_video);
 		},
 
-		queVideo: function() {
-			//werkt niet: this.player.cueVideoById(first_result.video_id);
+		cueVideo: function(video_id) {
+			this.queue.push(video_id);
 		},
+
+		ended: function() {
+			this.nextVideo();
+		},
+
+		removeVideo: function() {
+			this.queue.shift();
+		},
+
+		previousVideo: function() {
+			var prev_video = this.queue.unshift();
+			this.loadVideo(prev_video);
+		},
+
 
 	}
 });
